6daed87726a29abe65b990679933443c94d330ab
dfloriapallares <161695312+dfloriapallares@users.noreply.github.com>
2024-09-30 17:11:07 +0200

    Update MainActivity.java

--- ParametrosEntreActivities/app/src/main/java/com/example/parametrosentreactivities/MainActivity.java
+++ ParametrosEntreActivities/app/src/main/java/com/example/parametrosentreactivities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.parametrosentreactivities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,7 +28,9 @@ public class MainActivity extends AppCompatActivity {
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent navegarEntrePantallas = new Intent(getBaseContext(), MainActivity2.class);
+                navegarEntrePantallas.putExtra("USUARIO", edtUser.getText().toString());
+                startActivity(navegarEntrePantallas);
             }
         });
     }
