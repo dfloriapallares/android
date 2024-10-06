ce5c9eca8a1f3ce15655067ed9e136b8f04b110a
dfloriapallares <161695312+dfloriapallares@users.noreply.github.com>
2024-10-06 19:06:31 +0200

    Update MainActivity.java

--- Braveheart/app/src/main/java/com/example/braveheart/MainActivity.java
+++ Braveheart/app/src/main/java/com/example/braveheart/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.braveheart;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,35 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button miBotonA;
+    private Button miBotonB;
+    private Button miBotonC;
+    private Button miBotonD;
+    private Button miBotonE;
+    private Button miBotonF;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        inicializarComponentes();
+        miBotonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        "Boton A",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void inicializarComponentes(){
+        miBotonA = findViewById(R.id.btnA);
+        miBotonB = findViewById(R.id.btnB);
+        miBotonC = findViewById(R.id.btnC);
+        miBotonD = findViewById(R.id.btnD);
+        miBotonE = findViewById(R.id.btnE);
+        miBotonF = findViewById(R.id.btnF);
     }
 }
