c325fd112ea7f0e792c241abc3f2df3a54c4bceb
dfloriapallares <161695312+dfloriapallares@users.noreply.github.com>
2024-10-06 18:44:00 +0200

    .

--- TheMovieBD01/app/src/main/java/com/example/themoviedb01/json_mapper/MovieResponse.java
+++ TheMovieBD01/app/src/main/java/com/example/themoviedb01/json_mapper/MovieResponse.java
@@ -3,7 +3,6 @@ package com.example.themoviedb01.json_mapper;
 import java.util.List;
 
 public class MovieResponse {
-    //URL: https://api.themoviedb.org/3/movie/popular?api_key=3e05df20467dcc95d150c574db75e0fe
     private List<Movie> results;
 
     public List<Movie> getResults() {
